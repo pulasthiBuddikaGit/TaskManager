54d4547754b624a04254ddd8e86a7cabd82fda40
pulasthiBuddikaGit <balasooriyagepulasthi@gmail.com>
2025-06-15 03:06:57 +0530

    signup view created

---
+++ frontend/src/views/SignupView.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <!-- Left side animation -->
+    <div class="animation-box">
+      <img src="/animations/signup-task.gif" alt="Task Animation" class="animation" />
+    </div>
+
+    <!-- Right side form -->
+    <div class="form-box">
+      <SignupForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import SignupForm from '../components/SignupForm.vue'
+
+export default {
+  name: 'SignupView',
+  components: {
+    SignupForm
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 100vh;
+  padding: 1rem;
+  gap: 2rem;
+}
+
+.animation-box {
+  flex: 1;
+  text-align: center;
+}
+
+.animation {
+  width: 100%;
+  max-width: 400px;
+  height: auto;
+}
+
+.form-box {
+  flex: 1;
+  max-width: 400px;
+}
+
+@media screen and (max-width: 1024px) {
+  .container {
+    flex-direction: column;
+    justify-content: center;
+    padding: 2rem;
+  }
+
+  .animation-box, .form-box {
+    width: 100%;
+    text-align: center;
+  }
+
+  .animation {
+    max-width: 300px;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .animation {
+    max-width: 200px;
+  }
+}
+</style>
