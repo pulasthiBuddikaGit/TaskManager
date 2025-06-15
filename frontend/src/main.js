b9d1bd8ecf8e293ec25fdfb10b57bc42d53047ff
pulasthiBuddikaGit <balasooriyagepulasthi@gmail.com>
2025-06-15 05:52:20 +0530

    Refactor main.js to improve app initialization and organization

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,20 @@
-import './assets/main.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
-
 /*This is the entry point of your Vue app. It:
 Initializes the Vue app
 Mounts it to a DOM element (usually #app)
 Imports and uses things like the router or store */
 
+import './assets/main.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
-const app = createApp(App)
+// const app = createApp(App)
 
-app.use(router)
+// app.use(router)
 
-app.mount('#app')
+// app.mount('#app')
+
+createApp(App).use(store).use(router).mount('#app')
