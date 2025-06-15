38cc454829d4c583f3b3ec651a467e7e6a09a5ea
pulasthiBuddikaGit <balasooriyagepulasthi@gmail.com>
2025-06-15 10:23:41 +0530

    Update database configuration to set MongoDB as the default connection

--- backend/config/database.php
+++ backend/config/database.php
@@ -16,7 +16,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    // 'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -30,6 +31,12 @@ return [
     */
 
     'connections' => [
+
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'dsn'      => env('DB_DSN'),
+            'database' => env('DB_DATABASE'),
+        ],
 
         'sqlite' => [
             'driver' => 'sqlite',
@@ -110,12 +117,6 @@ return [
             'prefix_indexes' => true,
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
-        ],
-
-        'mongodb' => [
-            'driver'   => 'mongodb',
-            'dsn'      => env('DB_DSN'),
-            'database' => env('DB_DATABASE'),
         ],
 
     ],
