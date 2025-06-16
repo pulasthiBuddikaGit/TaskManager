0c332ad5192255f1ee1b3f78415b424cc42dee97
pulasthiBuddikaGit <balasooriyagepulasthi@gmail.com>
2025-06-16 16:33:07 +0530

    Add tasks module to Vuex store with state management and fetch action

---
+++ frontend/src/store/modules/tasks.js
@@ -0,0 +1,28 @@
+// store/modules/tasks.js
+import axios from 'axios'
+
+const state = () => ({
+  tasks: []
+})
+
+const mutations = {
+  setTasks(state, tasks) {
+    state.tasks = tasks
+  }
+}
+
+const actions = {
+  async fetchTasks({ commit, rootGetters }) {
+    const token = rootGetters['auth/token']
+    const res = await axios.get('/api/tasks', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    commit('setTasks', res.data)
+  }
+}
+
+const getters = {
+  tasks: state => state.tasks
+}
+
+export default { namespaced: true, state, mutations, actions, getters }
