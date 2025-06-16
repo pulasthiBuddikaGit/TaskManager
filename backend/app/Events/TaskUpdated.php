7d47555b71be1ec2e20a9934fa7f0ad0e341cb9c
pulasthiBuddikaGit <balasooriyagepulasthi@gmail.com>
2025-06-16 20:31:51 +0530

    Add TaskUpdated event for broadcasting task updates

---
+++ backend/app/Events/TaskUpdated.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Task;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+
+class TaskUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $task;
+
+    public function __construct(Task $task)
+    {
+        $this->task = $task;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('tasks');
+    }
+
+    public function broadcastAs()
+    {
+        return 'task.updated';
+    }
+}
