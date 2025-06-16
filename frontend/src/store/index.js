fa27e1277df8b81476f7bbc52ff559408b71542f
pulasthiBuddikaGit <balasooriyagepulasthi@gmail.com>
2025-06-16 16:32:53 +0530

    Add tasks and categories modules to Vuex store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore } from 'vuex'
 import auth from './modules/auth'
+import tasks from './modules/tasks'
+import categories from './modules/categories'
 
 export default createStore({
   modules: {
-    auth
+    auth,
+    tasks,
+    categories
   }
 })
 // This file sets up the Vuex store for state management in your Vue.js application.
